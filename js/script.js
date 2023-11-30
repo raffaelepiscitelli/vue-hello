e827ab6df86cab29e968f014d9bedf60147de99f
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-11-30 17:02:04 +0100

    Esercizio completo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const {createApp} = Vue;
+
+createApp ({
+    data(){
+        return{
+            title:"Gibson Les Paul Custom Black Beauty",
+            picture:"img/__static.gibson.com_product-images_Custom_CUSGJ5555_Ebony_3-Pickup_LPB357VOEBGH1_front.jpg"
+        }
+    },
+}).mount("#app");
